398e04f916179fdba9acc57bc800dc399bb3f318
Yuan Zhang <yuzh00007@uni-saarland.de>
2024-03-19 18:13:55 +0100

    adding argparse and passing in most values; update run.sh

---
+++ main.py
@@ -0,0 +1,48 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+from runners import run_abstract, run_twitter
+
+
+def main(sample_size, baseline, epoch, batch_size):
+    tokenizer = AutoTokenizer.from_pretrained("Hello-SimpleAI/chatgpt-detector-roberta", )
+    model = AutoModelForSequenceClassification.from_pretrained("./models/original")
+
+    run_twitter(model, tokenizer, sample_size, baseline, epoch, batch_size)
+    # run_abstract(model, tokenizer, sample_size, baseline)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-b", "--baseline",
+        help="whether to run off the shelf on test, True for yes, or False to train and ignore",
+        type=bool
+    )
+    parser.add_argument(
+        "-s", "--sample_size",
+        help="if you want to run with a subset of data (for testing, will be used for all three splits)",
+        type=int
+    )
+    parser.add_argument(
+        "-e", "--epoch",
+        help="number of epochs to run",
+        type=int
+    )
+    parser.add_argument(
+        "-p", "--per_device_train_batch_size",
+        help="number of epochs to run",
+        type=int
+    )
+    args = parser.parse_args()
+
+    import time
+    start = time.time()
+    main(
+        sample_size=args.sample_size,
+        baseline=args.baseline,
+        epoch=args.epoch,
+        batch_size=args.per_device_train_batch_size
+    )
+    print(f"total runtime: {time.time() - start:.2f}")
--- sic_cluster/run.sh
+++ sic_cluster/run.sh
@@ -4,5 +4,4 @@ echo $HOSTNAME
 which python
 
 #python utils/parse_trees.py
-python twitter_main.py
-#python abstract_main.py
+python main.py --sample_size 50 --baseline False --epoch 5 --per_device_train_batch_size 16
