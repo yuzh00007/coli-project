ab4520633c28151ad308d7a722170e7f7225d13d
Yuan Zhang <yuzh00007@uni-saarland.de>
2024-03-19 19:36:17 +0100

    add some alternate run types for run.sh

--- sic_cluster/run.sh
+++ sic_cluster/run.sh
@@ -4,4 +4,14 @@ echo $HOSTNAME
 which python
 
 #python utils/parse_trees.py
+
+# for testing
 python main.py --sample_size 100 --baseline False --epoch 2 --per_device_train_batch_size 16 --learning_rate 5e-5
+
+## actual runs
+#python main.py --baseline True --epoch 10 --per_device_train_batch_size 8 --learning_rate 5e-5
+#python main.py --baseline False --epoch 10 --per_device_train_batch_size 8 --learning_rate 5e-5
+#
+## actual runs
+#python main.py --baseline True --epoch 10 --per_device_train_batch_size 16 --learning_rate 5e-5
+#python main.py --baseline False --epoch 10 --per_device_train_batch_size 16 --learning_rate 5e-5
